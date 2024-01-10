97c2a3c8fd63d44a4f48c2323a2a3fc4d2aa797f
spctravis <spctravis@gmail.com>
2024-01-10 14:33:03 -0700

    e

--- GPOEnablePSlogging.ps1
+++ GPOEnablePSlogging.ps1
@@ -2,46 +2,45 @@
 Import-Module GroupPolicy
 
 # Define the name of the GPO
-$gpoName = "EnablePSLogging"
+$gpoName = "Default Domain Policy"
 
 # Define the hidden folder path
 $hiddenFolderPath = "$env:USERPROFILE\.PowerShellLogs"
 
 # Enable PowerShell Script Block Logging
 $scriptBlockLoggingKey = "HKLM\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"
-if (!(Get-GPRegistryValue -Name $gpoName -Key $scriptBlockLoggingKey -ValueName "EnableScriptBlockLogging")) {
+if (!(Get-GPRegistryValue -Name $gpoName -Key $scriptBlockLoggingKey -ValueName "EnableScriptBlockLogging" -ErrorAction SilentlyContinue)) {
     Set-GPRegistryValue -Name $gpoName -Key $scriptBlockLoggingKey -ValueName "EnableScriptBlockLogging" -Type DWord -Value 1
 }
 
 # Enable PowerShell Module Logging
 $moduleLoggingKey = "HKLM\Software\Policies\Microsoft\Windows\PowerShell\ModuleLogging"
-if (!(Get-GPRegistryValue -Name $gpoName -Key $moduleLoggingKey -ValueName "EnableModuleLogging")) {
+if (!(Get-GPRegistryValue -Name $gpoName -Key $moduleLoggingKey -ValueName "EnableModuleLogging" -ErrorAction SilentlyContinue)) {
     Set-GPRegistryValue -Name $gpoName -Key $moduleLoggingKey -ValueName "EnableModuleLogging" -Type DWord -Value 1
 }
 
 # Record all modules
-if (!(Get-GPRegistryValue -Name $gpoName -Key $moduleLoggingKey -ValueName "ModuleNames")) {
+if (!(Get-GPRegistryValue -Name $gpoName -Key $moduleLoggingKey -ValueName "ModuleNames" -ErrorAction SilentlyContinue)) {
     Set-GPRegistryValue -Name $gpoName -Key $moduleLoggingKey -ValueName "ModuleNames" -Type String -Value "*"
 }
 
 # Enable PowerShell Transcription
 $transcriptionKey = "HKLM\Software\Policies\Microsoft\Windows\PowerShell\Transcription"
-if (!(Get-GPRegistryValue -Name $gpoName -Key $transcriptionKey -ValueName "EnableTranscripting")) {
+if (!(Get-GPRegistryValue -Name $gpoName -Key $transcriptionKey -ValueName "EnableTranscripting" -ErrorAction SilentlyContinue)) {
     Set-GPRegistryValue -Name $gpoName -Key $transcriptionKey -ValueName "EnableTranscripting" -Type DWord -Value 1
 }
 
 # Include invocation headers
-if (!(Get-GPRegistryValue -Name $gpoName -Key $transcriptionKey -ValueName "IncludeInvocationHeader")) {
+if (!(Get-GPRegistryValue -Name $gpoName -Key $transcriptionKey -ValueName "IncludeInvocationHeader" -ErrorAction SilentlyContinue)) {
     Set-GPRegistryValue -Name $gpoName -Key $transcriptionKey -ValueName "IncludeInvocationHeader" -Type DWord -Value 1
 }
 
 # Set the output directory
-if (!(Get-GPRegistryValue -Name $gpoName -Key $transcriptionKey -ValueName "OutputDirectory")) {
+if (!(Get-GPRegistryValue -Name $gpoName -Key $transcriptionKey -ValueName "OutputDirectory" -ErrorAction SilentlyContinue)) {
     Set-GPRegistryValue -Name $gpoName -Key $transcriptionKey -ValueName "OutputDirectory" -Type String -Value $hiddenFolderPath
 }
-
-# Enable auditing of process creation (Event ID 4688)
-$auditProcessCreationKey = "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Audit\ProcessCreation"
-if (!(Get-GPRegistryValue -Name $gpoName -Key $auditProcessCreationKey -ValueName "IncludeCmdLine")) {
-    Set-GPRegistryValue -Name $gpoName -Key $auditProcessCreationKey -ValueName "IncludeCmdLine" -Type DWord -Value 1
+# Enable WMI Activity Logging
+$wmiLoggingKey = "HKLM\SOFTWARE\Microsoft\WBEM\CIMOM\Logging"
+if (!(Get-GPRegistryValue -Name $gpoName -Key $wmiLoggingKey -ValueName "Logging" -ErrorAction SilentlyContinue)) {
+    Set-GPRegistryValue -Name $gpoName -Key $wmiLoggingKey -ValueName "Logging" -Type DWord -Value 2
 }
